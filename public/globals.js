7e60ebecfb0006247ca40a48cafed82ea84a50d1
Xian Kai Ng <ngxiankai286@gmail.com>
2017-07-12 10:26:26 -0700

    committing ignorefile

---
+++ public/globals.js
@@ -0,0 +1,23 @@
+  //GLOBAL CONSTS
+	var BLUE = '#3A5BCD';
+	var RED = '#EF2B36';
+	var YELLOW = '#FFC636';
+	var GREEN = '#02A817';
+	var BLACK = '#000000';
+	var SPACE = 32;
+	var P = 80;
+	var L = 76;
+	var Q = 81;
+	var ESC = 27;
+	var A = 65;
+
+	//Document vars
+var canvas = document.getElementById('myCanvas');
+
+//game specific VARS
+		var started  = false;
+		var ballRadius = 10;
+		var paddleWidth = 10;
+		var paddleHeight = 400;
+		var paddleSpeed = 0.7;
+		var numballs = 2;
