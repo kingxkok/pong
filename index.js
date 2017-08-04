44107f624ad1e7a7c840e935b6293cef85a2221e
Xian Kai Ng <ngxiankai286@gmail.com>
2017-08-03 20:10:52 -0700

    choose which mentees game

--- index.js
+++ index.js
@@ -6,10 +6,42 @@ var fs = require('fs');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    fs.readFile(__dirname + '/pong.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
+
+app.get('/pong_kai', function(req, res) {
+    fs.readFile(__dirname + '/pong_kai.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+app.get('/pong_victor', function(req, res) {
+    fs.readFile(__dirname + '/pong_victor.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+app.get('/pong_jordan', function(req, res) {
+    fs.readFile(__dirname + '/pong_jordan.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+app.get('/pong_luke', function(req, res) {
+    fs.readFile(__dirname + '/pong_luke.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+app.get('/pong_frost', function(req, res) {
+    fs.readFile(__dirname + '/pong_frost.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+
 
 app.listen(port);
 console.log("PONG is listening on port ",port);
