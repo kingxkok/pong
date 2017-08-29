bef3536b9b264a8292313992b5a8722b9f415a23
Xian Kai Ng <ngxiankai286@gmail.com>
2017-08-29 12:09:32 -0700

    sync with heroku

--- index.js
+++ index.js
@@ -6,37 +6,37 @@ var fs = require('fs');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/views/index.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
 
 app.get('/pong_kai', function(req, res) {
-    fs.readFile(__dirname + '/pong_kai.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/views/pong_kai.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
 
 app.get('/pong_victor', function(req, res) {
-    fs.readFile(__dirname + '/pong_victor.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/views/pong_victor.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
 
 app.get('/pong_jordan', function(req, res) {
-    fs.readFile(__dirname + '/pong_jordan.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/views/pong_jordan.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
 
 app.get('/pong_luke', function(req, res) {
-    fs.readFile(__dirname + '/pong_luke.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/views/pong_luke.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
 
 app.get('/pong_frost', function(req, res) {
-    fs.readFile(__dirname + '/pong_frost.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/views/pong_frost.html', 'utf8', function(err, text){
         res.send(text);
     });
 });
